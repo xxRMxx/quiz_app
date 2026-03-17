d2f7042695bbe4851d2346be3788813f29a1f2ef
Raphael Meier <raphaelm@ranger.de>
2026-03-18 00:55:24 +0100

    release of beta beta version

--- quiz_project/settings.py
+++ quiz_project/settings.py
@@ -114,9 +114,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+STATICFILES_DIRS = []
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
